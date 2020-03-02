c5d668a2fea174d9d84c24583cdda471cc452010
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-01 18:14:00 -0600

    Finally, maybe I can improve mazes but it works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::API
     include ActionController::Serialization
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+
+    before_action :authenticate
+
+    protected
+
+    def authenticate
+        authenticate_token || render_unauthorized
+    end
+
+    def authenticate_token
+        authenticate_with_http_token do |token, options|
+            return token == "f98e847a-03bc-43d3-8dfa-1f192902b15a"
+        end
+    end
+
+    def render_unauthorized(realm = "Application")
+        self.headers["WWW-Authenticate"] = %(Token realm="#{realm}")
+        render json: 'Bad credentials', status: :unauthorized
+    end
 end
